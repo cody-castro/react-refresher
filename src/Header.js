7be961d7dfdb9f681a2a65cb99690ccff91d33c1
Cody <codyicastro@gmail.com>
2022-01-06 09:36:26 -0500

    added logo,header and home components

---
+++ src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import "./header.css"
+
+function Header() {
+    return(
+        <div className='header'>
+            <img className='header__icon' src="airbnblogo.png" alt="logo"/>
+            <h1> Header Component </h1>
+            
+        </div>
+    )
+}
+
+export default Header
