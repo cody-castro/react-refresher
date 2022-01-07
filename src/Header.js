ab12379447f0175df8e0e830a3b0524128d36788
Cody <codyicastro@gmail.com>
2022-01-07 11:05:00 -0500

    finished banner,added background photo, set state in banner

--- src/Header.js
+++ src/Header.js
@@ -1,12 +1,27 @@
 import React from 'react'
 import "./header.css"
+import SearchIcon from '@material-ui/icons/Search';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import LanguageIcon from "@material-ui/icons/Language"
+import {Avatar} from "@material-ui/core"
 
 function Header() {
     return(
         <div className='header'>
             <img className='header__icon' src="airbnblogo.png" alt="logo"/>
-            <h1> Header Component </h1>
+
             
+            <div className='header__center'>
+                <input type='text'/>
+                <SearchIcon />
+                <LanguageIcon />
+                <Avatar />
+            </div>
+
+            <div className='header__right'>
+                <p>Become a host</p>
+            </div>
+
         </div>
     )
 }
--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./home.css"
+import Banner from "./Banner.js"
 
 function Home() {
     return(
         <div className='home'>
-            <h1> Home Component </h1>
+            {/* <h1> Home Component </h1> */}
+            <Banner/>
         </div>
     )
 }
