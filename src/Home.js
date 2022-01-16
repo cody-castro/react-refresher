6c5030d181c27bebfe2f57d09ad0720ecc62c28d
Cody <codyicastro@gmail.com>
2022-01-16 14:07:47 -0500

    playing with faker

--- src/Home.js
+++ src/Home.js
@@ -5,6 +5,13 @@ import Card from "./Card.js"
 import faker from '@faker-js/faker'
 
 function Home() {
+
+    function price(){
+        return (
+            faker.finance.price()
+        )
+    }
+
     return(
         <div className='home'>
             <Banner/>
@@ -12,9 +19,9 @@ function Home() {
             <div className='home__section'>
                 
                 <Card
-                    src="https://a0.muscache.com/im/pictures/eb9c7c6a-ee33-414a-b1ba-14e8860d59b3.jpg?im_w=720"
+                    src={faker.image.imageUrl()}
                     title="Online Experiences"
-                    description={faker.company.bs()}
+                    description={faker.hacker.phrase()}
                 />
                   <Card
                     src="https://a0.muscache.com/im/pictures/15159c9c-9cf1-400e-b809-4e13f286fa38.jpg?im_w=720"
@@ -34,19 +41,19 @@ function Home() {
                     src="https://media.nomadicmatt.com/2019/airbnb_breakup3.jpg"
                     title="3 Bedroom Flat in Bournemouth"
                     description="Superhost with a stunning view of the beachside in Sunny Bournemouth"
-                    price="£130/night"
+                    price={faker.finance.amount()}
                 />
                 <Card
                     src="https://thespaces.com/wp-content/uploads/2017/08/Courtesy-of-Airbnb.jpg"
                     title="Penthouse in London"
                     description="Enjoy the amazing sights of London with this stunning penthouse"
-                    price="£350/night"
+                    price={faker.finance.amount()}
                 />
                 <Card
                     src="https://media.nomadicmatt.com/2018/apartment.jpg"
                     title="1 Bedroom apartment"
                     description="Superhost with great amenities and a fabolous shopping complex nearby"
-                    price="£700/night"
+                    price={faker.finance.amount()}
                 /> 
                
 
