9701a211197ef0324498787a2fb5375e5a610959
Cody <codyicastro@gmail.com>
2022-01-17 21:00:34 -0500

    fixed routes yay thanks Catherine

--- src/App.js
+++ src/App.js
@@ -3,25 +3,26 @@ import './App.css';
 import Home from './Home.js'
 import Header from './Header.js'
 import Footer from './Footer.js'
+import SearchPage from './SearchPage'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
 
     // BEM
     <div className="App">
+      <Router>
+        <Header />
 
-    <Header />
-    <Home />
-    <Footer/>
-    {/* Home */}
-      {/* Header */}
-      {/* banner */}
-          {/* search */}
-      {/* cards */}
-      {/* footer */}
-    {/* SearchPage */}
-      {/* Header */}
-      {/* ... */}
+        <Routes>
+          <Route path="search" element={<SearchPage/>} />
+          <Route path="/" element={<Home/>}/>
+        </Routes>
+        
+        <Footer/>
+
+      </Router>
+
     </div>
   );
 }
---
+++ src/SearchPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './searchPage.css'
+
+
+function SearchPage(){
+
+    return(
+        <h1>I am Search Page</h1>
+    )
+
+}
+
+export default SearchPage
