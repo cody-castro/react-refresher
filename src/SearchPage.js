6f0d9bad17265549d9082a78d2aadcb69a927554
Cody <codyicastro@gmail.com>
2022-01-19 09:25:34 -0500

    getting search page built

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './searchPage.css'
+import {Button} from "@material-ui/core"
+import SearchResult from './SearchResult.js'
 
 
 function SearchPage(){
@@ -7,9 +9,15 @@ function SearchPage(){
     return(
         <div className='searchPage'>
             <div className='searchPage__info'>
-                <p>62 Wtays - 26 August to 30 August - 2 Guests</p>
+                <p>62 Stays - 26 August to 30 August - 2 Guests</p>
                 <h1>Stays nearby</h1>
+                <Button variant="outlined">Type of Place</Button>
+                <Button variant="outlined">Price</Button>
+                <Button variant="outlined">Rooms and Beds</Button>
+                <Button variant="outlined">More filters</Button>
+
             </div>
+            <SearchResult />
 
         </div>
     )
---
+++ src/SearchResult.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './searchResult.css'
+
+function SearchResult(){
+
+    return(
+        <div className='searchResult'>
+
+        </div>
+    )
+
+
+}
+
+export default SearchResult
